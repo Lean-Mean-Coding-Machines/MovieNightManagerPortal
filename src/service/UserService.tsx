bea493402d2c4bd10b2d339f88e8689e867da269
hzyniecki <hzyniecki@gmail.com>
2023-04-23 10:06:06 -0500

    Fixed shit code

--- src/service/UserService.tsx
+++ src/service/UserService.tsx
@@ -2,11 +2,16 @@ import http from "../http-common";
 import IMnmApiResponse from "../model/IMnmApiResponse";
 import IUserCreateRequest from "../model/user/IUserCreateRequest";
 import IUserCreateResponse from "../model/user/IUserCreateResponse";
+import IUsers from "../model/IMovieNightSegment";
 
 const createNewUser = (userRequest: IUserCreateRequest) => {
     return http.post<IMnmApiResponse<IUserCreateResponse>>("/user/create", userRequest);
 };
 
-const UserService = {createNewUser};
+const getAllUsers = () => {
+    return http.get<IMnmApiResponse<IUsers>>("/user/all");
+};
+
+const UserService = {createNewUser,getAllUsers};
 
 export default UserService;
