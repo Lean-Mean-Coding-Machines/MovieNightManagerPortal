291797263b2e414bae4e55f0ce70343d62e9be47
hzyniecki <hzyniecki@gmail.com>
2023-04-23 11:11:34 -0500

    fixed path

--- src/service/UserService.tsx
+++ src/service/UserService.tsx
@@ -2,7 +2,7 @@ import http from "../http-common";
 import IMnmApiResponse from "../model/IMnmApiResponse";
 import IUserCreateRequest from "../model/user/IUserCreateRequest";
 import IUserCreateResponse from "../model/user/IUserCreateResponse";
-import IUsers from "../model/IMovieNightSegment";
+import IUsers from "../model/IUsers";
 
 const createNewUser = (userRequest: IUserCreateRequest) => {
     return http.post<IMnmApiResponse<IUserCreateResponse>>("/user/create", userRequest);
