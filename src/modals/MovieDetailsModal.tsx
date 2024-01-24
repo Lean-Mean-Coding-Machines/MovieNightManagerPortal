b43dff58613b1863202b0862999c953089f056db
hzyniecki <hzyniecki@gmail.com>
2024-01-23 21:51:00 -0600

    fixed dupe

--- src/modals/MovieDetailsModal.tsx
+++ src/modals/MovieDetailsModal.tsx
@@ -13,7 +13,7 @@ interface ModalType {
 
   const modalStyle = {
     position: 'absolute' as 'absolute',
-    top: {xs: '50%', lg: '50%'},
+    top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     display: 'block',
